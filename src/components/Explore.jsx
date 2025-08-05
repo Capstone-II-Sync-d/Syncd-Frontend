d54f0e02756f8c121e8667ffc450ad54db4c0570
Joseph Collado <sourpina@gmail.com>
2025-08-05 14:14:32 -0400

    #2 Added logic to get and set events

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { API_URL } from "../shared";
 
 const Explore = () => {
   const [events, setEvents] = useState([]);
   const [businesses, setBusinesses] = useState([]);
 
   const getEvents = async () => {
-
+    try {
+      const response = await axios.get(`${API_URL}/api/calendarItems/events`);
+      setEvents(response.data);
+      console.log(`Successfully retrieved ${response.data.length} events`);
+    } catch (error) {
+      console.error("Error getting events:", error);
+    }
   };
 
   const getBusinesses = async () => {
 
   };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
 
   return (
     <div className="explore-container">
