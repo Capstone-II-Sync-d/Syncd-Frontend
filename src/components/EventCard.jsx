f8b22fe3bc793efdf8210affa29c66fe7b844a44
Joseph Collado <sourpina@gmail.com>
2025-08-05 14:57:07 -0400

    #2 Added event details

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import "./EventCardStyles.css";
 
 const EventCard = ({ event }) => {
   return (
     <div className="event-card">
-      <p>{event.title}</p>
+      <h3 className="event-title">{event.title}</h3>
+      <p className="description">{event.description}</p>
+      <p className="hostname">
+        <strong>Hosted By: </strong>
+        {
+          event.business ? (
+            event.business
+          ) : (
+            event.creatorUsername
+          )
+        }
+      </p>
+      <p className="start-time">
+        <strong>Start:</strong> {event.startTime}
+      </p>
+      <p className="end-time">
+        <strong>End:</strong> {event.endTime}
+      </p>
     </div>
   );
 };
