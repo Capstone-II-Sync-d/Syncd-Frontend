608fc0f35323e3dad842008c19e6fe416c16a20a
Joseph Collado <sourpina@gmail.com>
2025-08-06 15:25:19 -0400

    #2 Reorganized business card info

--- src/components/BusinessCard.jsx
+++ src/components/BusinessCard.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./BusinessCardStyle.css";
 
 const BusinessCard = ({ business }) => {
   if (!business) return null;
 
   return (
-    <Link to={`/business/profile/${business.id}`}>
-      <div className="businessCard">
-        <img src={business.pictureUrl} className="businessCardPic" />
-        <div className="businessCardInfo">
-          <h2>{business.name}</h2>
-          {business.bio && <p className="businessCardBio">{business.bio}</p>}
-        </div>
+    <Link to={`/business/profile/${business.id}`} className="business-card">
+      <div className="header">
+        <img src={business.icon} className="icon" />
+        <h2>{business.name}</h2>
+      </div>
+      <div className="info">
+        {
+          business.owner && 
+          <p><strong>Owner:</strong> {business.owner}</p>
+        }
+        <p className="bio">{business.bio}</p>
       </div>
     </Link>
   );
