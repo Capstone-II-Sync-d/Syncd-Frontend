a01f8b448c1643fd6a2ad9814790322fec7c1644
Guarotav <guarotav@gmail.com>
2025-08-11 12:00:50 -0400

    #7 Creation of Following, Followers, MyBuisinesses, and Friends List
    After creating list components for the My Businesses , user Following, and User Friends, i merged with explore page and main page, I sectioned out the list files. I made a new folder for lists and a styling folder in that folder for css.
    
    These lists included fetching of all businesses a uyser owns, all friends of a specific user, all businesses a user follows, and all followers of a business. Each containing a search nar that queries through the information that is feched and listed.

---
+++ src/components/Lists/BusinessList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BusinessCard from "../Cards/BusinessCard";
+
+const BusinessList = ({ businesses }) => {
+  return businesses.length > 0 ? (
+    businesses.map((business) => (
+      <BusinessCard key={business.id} business={business} />
+    ))
+  ) : (
+    <p> No businesses found </p>
+  );
+};
+
+export default BusinessList;
---
+++ src/components/Lists/styling/EventList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EventCard from "./EventCard";
+
+const EventList = ({ events }) => {
+  return (
+    events.length > 0 ? (
+      events.map((event) => (<EventCard key={event.id} event={event} />))
+    ) : (
+      <p> No events found </p>
+    )
+  );
+};
+
+export default EventList;
