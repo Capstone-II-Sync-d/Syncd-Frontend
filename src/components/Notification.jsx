9d469d8288e3ad60c323e0ec653042d6aab5ea6e
Joseph Collado <sourpina@gmail.com>
2025-08-13 14:13:42 -0400

    #11 Added conditional button rendering

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Notification = ({ notification }) => {
   const [message, setMessage] = useState("");
+  const [showButtons, setShowButtons] = useState(false);
 
   const generateRequestMessage = (notif) => {
     const otherUserName = `${notif.otherUser.firstName} (${notif.otherUser.username})`;
@@ -18,6 +19,7 @@ const Notification = ({ notification }) => {
         setMessage("Blank notification");
         return;
       case 'request':
+        setShowButtons(notification.status !== "accepted");
         setMessage(generateRequestMessage(notification));
         return;
     }
@@ -29,6 +31,10 @@ const Notification = ({ notification }) => {
     >
       <div className="notification-message">
         {message}
+      </div>
+      <div className="notification-buttons">
+        <button>Accept</button>
+        <button>Decline</button>
       </div>
       <div className="notification-time">
         {notification.time}
